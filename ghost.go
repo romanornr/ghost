60f5c9c79f4409429717f5f291bb913098d44306
romanornr <romanornr@gmail.com>
2024-07-10 03:41:21 +0200

    temp use old code

--- ghost.go
+++ ghost.go
@@ -1,9 +1,13 @@
 package ghost
 
 import (
+	"bytes"
 	"encoding/hex"
+	"encoding/json"
 	"fmt"
-	"github.com/golang-jwt/jwt/v5"
+	"github.com/gbrlsnchs/jwt/v3"
+	_ "github.com/golang-jwt/jwt/v5"
+	"io"
 	"net/http"
 	"strings"
 	"time"
@@ -18,41 +22,69 @@ const (
 	httpClientTimeout     = 10 * time.Second
 )
 
+//type Ghost struct {
+//	AdminAPIToken      string
+//	ContentAPIToken    string
+//	URL                string
+//	jwtToken           string
+//	jwtTokenExpiration time.Time
+//	client             *http.Client
+//}
+
 type Ghost struct {
-	AdminAPIToken      string
-	ContentAPIToken    string
-	URL                string
+	adminAPIToken      string
+	contentAPIToken    string
 	jwtToken           string
 	jwtTokenExpiration time.Time
-	client             *http.Client
+	url                string
 }
 
-func NewGhost(url, contentAPIToken, adminAPIToken string) (*Ghost, error) {
-	if url == "" || contentAPIToken == "" || adminAPIToken == "" {
-		return nil, fmt.Errorf("url, contentAPIToken and adminAPIToken are required")
-	}
+//func NewGhost(url, contentAPIToken, adminAPIToken string) (*Ghost, error) {
+//	if url == "" || contentAPIToken == "" || adminAPIToken == "" {
+//		return nil, fmt.Errorf("url, contentAPIToken and adminAPIToken are required")
+//	}
+//	return &Ghost{
+//		URL:             url,
+//		ContentAPIToken: contentAPIToken,
+//		AdminAPIToken:   adminAPIToken,
+//		client:          &http.Client{Timeout: httpClientTimeout},
+//	}, nil
+//}
+
+func New(url, contentAPIToken, adminAPIToken string) *Ghost {
 	return &Ghost{
-		URL:             url,
-		ContentAPIToken: contentAPIToken,
-		AdminAPIToken:   adminAPIToken,
-		client:          &http.Client{Timeout: httpClientTimeout},
-	}, nil
+		adminAPIToken:   adminAPIToken,
+		contentAPIToken: contentAPIToken,
+		url:             url,
+	}
 }
 
-func (g *Ghost) checkAndRenewJWTToken() error {
-	//if g.jwtToken == "" || g.jwtTokenExpiration.Before(time.Now()) {
-	//	jwtToken, jwtTokenExpiration, err := g.getJWTToken()
-	//	if err != nil {
-	//		return fmt.Errorf("failed to get JWT token: %v", err)
-	//	}
-	//}
-	//g.jwtToken = jwtToken
-	//g.jwtTokenExpiration = jwtTokenExpiration
-	//return nil
+//func (g *Ghost) checkAndRenewJWTToken() error {
+//	//if g.jwtToken == "" || g.jwtTokenExpiration.Before(time.Now()) {
+//	//	jwtToken, jwtTokenExpiration, err := g.getJWTToken()
+//	//	if err != nil {
+//	//		return fmt.Errorf("failed to get JWT token: %v", err)
+//	//	}
+//	//}
+//	//g.jwtToken = jwtToken
+//	//g.jwtTokenExpiration = jwtTokenExpiration
+//	//return nil
+//	if g.jwtToken == "" || g.jwtTokenExpiration.Before(time.Now()) {
+//		jwtToken, jwtTokenExpiration, err := g.getJWTToken()
+//		if err != nil {
+//			return fmt.Errorf("failed to generate JWT: %w", err)
+//		}
+//		g.jwtToken = jwtToken
+//		g.jwtTokenExpiration = jwtTokenExpiration
+//	}
+//	return nil
+//}
+
+func (g *Ghost) checkAndRenewJWT() error {
 	if g.jwtToken == "" || g.jwtTokenExpiration.Before(time.Now()) {
-		jwtToken, jwtTokenExpiration, err := g.getJWTToken()
+		jwtToken, jwtTokenExpiration, err := generateJWT(g.adminAPIToken)
 		if err != nil {
-			return fmt.Errorf("failed to generate JWT: %w", err)
+			return err
 		}
 		g.jwtToken = jwtToken
 		g.jwtTokenExpiration = jwtTokenExpiration
@@ -60,42 +92,144 @@ func (g *Ghost) checkAndRenewJWTToken() error {
 	return nil
 }
 
-func (g *Ghost) getJWTToken() (string, time.Time, error) {
-	keyParts := strings.Split(g.AdminAPIToken, ":")
+//func generateJWT(keyID string) (string, time.Time, error) {
+//	keyParts := strings.Split(keyID, ":")
+//	if len(keyParts) != 2 {
+//		return "", time.Time{}, fmt.Errorf("invalid admin API token format")
+//	}
+//
+//	id := keyParts[0]
+//	rawSecret := keyParts[1]
+//	secret := make([]byte, hex.DecodedLen(len(rawSecret)))
+//	_, err := hex.Decode(secret, []byte(rawSecret))
+//	if err != nil {
+//		return "", time.Time{}, fmt.Errorf("failed to decode secret: %v", err)
+//	}
+//
+//	now := time.Now()
+//	expirationTime := now.Add(tokenExpirationPeriod)
+//
+//	//token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+//	//	"Audience":       jwt.WithAudience(jwtAudience),
+//	//	"ExpirationTime": jwt.NewNumericDate(expirationTime),
+//	//	"IssuedAt":       jwt.NewNumericDate(now),
+//	//})
+//
+//	claims := jwt.RegisteredClaims{
+//		Audience:  jwt.ClaimStrings{jwtAudience},
+//		ExpiresAt: jwt.NewNumericDate(expirationTime),
+//		IssuedAt:  jwt.NewNumericDate(now),
+//		ID:        id,
+//	}
+//
+//	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+//
+//	tokenString, err := token.SignedString(secret)
+//	if err != nil {
+//		return "", time.Time{}, fmt.Errorf("failed to sign token: %v", err)
+//	}
+//
+//	return tokenString, expirationTime, nil
+//}
+
+func generateJWT(keyID string) (string, time.Time, error) {
+	var now = time.Now()
+	var expiration = now.Add(5 * time.Minute)
+
+	keyParts := strings.Split(keyID, ":")
 	if len(keyParts) != 2 {
-		return "", time.Time{}, fmt.Errorf("invalid admin API token format")
+		return "", expiration, fmt.Errorf("invalid Client.Key format")
 	}
-
 	id := keyParts[0]
-	rawSecret := keyParts[1]
+	rawSecret := []byte(keyParts[1])
 	secret := make([]byte, hex.DecodedLen(len(rawSecret)))
-	_, err := hex.Decode(secret, []byte(rawSecret))
+	_, err := hex.Decode(secret, rawSecret)
 	if err != nil {
-		return "", time.Time{}, fmt.Errorf("failed to decode secret: %v", err)
+		return "", expiration, err
 	}
 
-	now := time.Now()
-	expirationTime := now.Add(tokenExpirationPeriod)
-
-	//token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-	//	"Audience":       jwt.WithAudience(jwtAudience),
-	//	"ExpirationTime": jwt.NewNumericDate(expirationTime),
-	//	"IssuedAt":       jwt.NewNumericDate(now),
-	//})
-
-	claims := jwt.RegisteredClaims{
-		Audience:  jwt.ClaimStrings{jwtAudience},
-		ExpiresAt: jwt.NewNumericDate(expirationTime),
-		IssuedAt:  jwt.NewNumericDate(now),
-		ID:        id,
+	hs256 := jwt.NewHS256(secret)
+	p := jwt.Payload{
+		Audience:       jwt.Audience{"/v5/admin/"},
+		ExpirationTime: jwt.NumericDate(expiration),
+		IssuedAt:       jwt.NumericDate(now),
 	}
-
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-
-	tokenString, err := token.SignedString(secret)
+	token, err := jwt.Sign(p, hs256, jwt.KeyID(id))
 	if err != nil {
-		return "", time.Time{}, fmt.Errorf("failed to sign token: %v", err)
+		return "", expiration, err
+	}
+	return string(token), expiration, nil
+}
+
+var myClient = &http.Client{Timeout: 10 * time.Second}
+
+func (g *Ghost) getJson(url string, target interface{}) error {
+	if err := g.checkAndRenewJWT(); err != nil {
+		return err
 	}
 
-	return tokenString, expirationTime, nil
+	req, err := http.NewRequest(http.MethodGet, url, bytes.NewBuffer([]byte{}))
+	if err != nil {
+		return err
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", "Ghost"+" "+g.jwtToken)
+	resp, err := myClient.Do(req)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if err := resp.Body.Close(); err != nil {
+			fmt.Printf("Cannot close body reader: %v\n", err)
+		}
+	}()
+
+	err = parsePostResponse(resp, err, target)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func parsePostResponse(resp *http.Response, err error, target interface{}) error {
+	content, _ := io.ReadAll(resp.Body)
+	responseBody := string(content[:])
+
+	if resp.StatusCode != http.StatusOK && resp.StatusCode != http.StatusCreated {
+		return fmt.Errorf("unexpected status code %d: %s", resp.StatusCode, responseBody)
+	}
+
+	err = json.Unmarshal(content, target)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (g *Ghost) postJson(url string, data []byte, target interface{}) error {
+	req, err := http.NewRequest(http.MethodPost, url, bytes.NewBuffer(data))
+	if err != nil {
+		return err
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", "Ghost"+" "+g.jwtToken)
+	resp, err := myClient.Do(req)
+	if err != nil {
+		return err
+	}
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			fmt.Printf("Error closing body: %v\n", err)
+		}
+	}(resp.Body)
+
+	err = parsePostResponse(resp, err, target)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,13 @@ module github.com/romanornr/ghost
 
 go 1.22.4
 
-require github.com/golang-jwt/jwt/v5 v5.2.1 // indirect
+require (
+	github.com/gbrlsnchs/jwt/v3 v3.0.1
+	github.com/golang-jwt/jwt/v5 v5.2.1
+)
+
+require (
+	github.com/magefile/mage v1.9.0 // indirect
+	golang.org/x/crypto v0.0.0-20190927123631-a832865fa7ad // indirect
+	golang.org/x/xerrors v0.0.0-20191204190536-9bdfabe68543 // indirect
+)
