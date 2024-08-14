7b7c4f59c856e70f3de68a3c633806115feeaa3b
romanornr <romanornr@gmail.com>
2024-08-14 04:12:46 +0200

    Add ClientOptions for API keys and custom HTTP client
    Introduce new ClientOptions: `withContentAPIKey`, `withAdminAPIKey`, and `withHTTPClient`. These options enable users to set content and admin API keys and customize the HTTP client.

--- ghost.go
+++ ghost.go
@@ -42,3 +42,22 @@ func NewClient(baseURL string, opts ...ClientOption) Client {
 	}
 	return c
 }
+
+func withContentAPIKey(key string) ClientOption {
+	return func(c *client) {
+		c.contentAPIKey = key
+	}
+}
+
+func withAdminAPIKey(key string) ClientOption {
+	return func(c *client) {
+		c.adminAPIKey = key
+	}
+}
+
+func withHTTPClient(httpClient *http.Client) ClientOption {
+	// Return a function that sets the HTTP client on the client
+	return func(c *client) {
+		c.httpClient = httpClient
+	}
+}
